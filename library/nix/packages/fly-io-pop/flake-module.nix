bb301cbb25878570bf49f2061b096bc918913578
Louis Opter <louis@opter.org>
2024-11-11 03:00:08 +0000

    fly-io-pop: enable http2 and proxy caching globally
    GPT4o tells me that if there is no way to determine the expiry of a
    proxied response, then Nginx won't cache it.

--- library/nix/packages/fly-io-pop/flake-module.nix
+++ library/nix/packages/fly-io-pop/flake-module.nix
@@ -283,6 +283,7 @@ in
             events {
             }
             http {
+              http2 on;
               # Load mime types and configure maximum size of the types hash tables.
               include ${pkgs.mailcap}/etc/nginx/mime.types;
               types_hash_max_size 2688;
@@ -316,6 +317,8 @@ in
               gzip_proxied expired no-cache no-store private auth;
               gzip_types application/atom+xml application/geo+json application/javascript application/json application/ld+json application/manifest+json application/rdf+xml application/vnd.ms-fontobject application/wasm application/x-rss+xml application/x-web-app-manifest+json application/xhtml+xml application/xliff+xml application/xml font/collection font/otf font/ttf image/bmp image/svg+xml image/vnd.microsoft.icon text/cache-manifest text/calendar text/css text/csv text/javascript text/markdown text/plain text/vcard text/vnd.rim.location.xloc text/vtt text/x-component text/xml;
               proxy_cache_path /var/cache/nginx levels=1:2 keys_zone=pop-cache:100m max_size=1g inactive=182d use_temp_path=off;
+              proxy_cache_key $scheme$host$request_uri;
+              proxy_cache pop-cache;
               proxy_redirect          off;
               proxy_connect_timeout   3s;
               proxy_send_timeout      3s;
