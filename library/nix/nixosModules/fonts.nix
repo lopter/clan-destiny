b97ace1ab31977cb0a0fe2ec8cc6e46a73ad0fe4
Louis Opter <louis@opter.org>
2025-01-18 16:06:50 +0000

    home-kal: add basic Microsoft fonts
    I am trying to fix µ (as in µF) not being displayed by Okular in some
    technical documentation.
    
    µ was still not displayed by Okular when starting from a `nix-shell -p
    '[ corefonts vistafonts ]'`. Maybe I actually need to apply the changes
    or those fonts are not enough to fix the problem, but it seems like a
    sensible thing to have them anyway.

--- library/nix/nixosModules/fonts.nix
+++ library/nix/nixosModules/fonts.nix
@@ -1,10 +1,17 @@
 { pkgs, ... }:
 {
+  clan-destiny.nixpkgs.unfreePredicates = [
+    "corefonts"
+    "vistafonts"
+  ];
+
   fonts.packages = with pkgs; [
+    corefonts
     inconsolata
     nerd-fonts.bitstream-vera-sans-mono
     nerd-fonts.fira-mono
     nerd-fonts.inconsolata
     nerd-fonts.monofur
+    vistafonts
   ];
 }
