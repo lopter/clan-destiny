17a89b017f61b596103588fc0dfa21c3569ee53a
Louis Opter <louis@opter.org>
2025-10-21 06:26:51 +0000

    nixpkgs: add insecurePackages option
    To be used with mindforger.

--- library/nix/nixosModules/nixpkgs.nix
+++ library/nix/nixosModules/nixpkgs.nix
@@ -1,16 +1,29 @@
 { config, lib, ... }:
 {
-  options.clan-destiny.nixpkgs.unfreePredicates = lib.mkOption {
-    description = ''
-      The list of unfree package names that are allowed for installation.
-    '';
-    type = lib.types.listOf lib.types.nonEmptyStr;
-    default = [ ];
+  options.clan-destiny.nixpkgs = {
+    unfreePredicates = lib.mkOption {
+      description = ''
+        The list of unfree package names that are allowed for installation.
+      '';
+      type = lib.types.listOf lib.types.nonEmptyStr;
+      default = [ ];
+    };
+    insecurePackages = lib.mkOption {
+      description = ''
+        The list of insecure package names that are allowed for installation.
+      '';
+      type = lib.types.listOf lib.types.nonEmptyStr;
+      default = [ ];
+    };
   };
 
-  config.nixpkgs.config.allowUnfreePredicate =
-    let
-      unfreePredicates = config.clan-destiny.nixpkgs.unfreePredicates;
-    in
-    pkg: builtins.elem (lib.getName pkg) unfreePredicates;
+  config.nixpkgs.config =
+  let
+    allowed = allowList: pkg: builtins.elem (lib.getName pkg) allowList;
+    cfg = config.clan-destiny.nixpkgs;
+  in
+  {
+    allowUnfreePredicate = allowed cfg.unfreePredicates;
+    allowInsecurePredicate = allowed cfg.insecurePackages;
+  };
 }
